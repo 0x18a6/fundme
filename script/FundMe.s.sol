ae915552f2eab520aed219946c766c4c62fff704
0x18a6 <huffcbytecode@gmail.com>
2023-12-02 14:30:38 +0100

    test on sepolia

--- script/FundMe.s.sol
+++ script/FundMe.s.sol
@@ -1,4 +1,4 @@
-// SPDX-LICENSE-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
 import {Script} from "forge-std/Script.sol";
--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -1,4 +1,4 @@
-// SPDX-LICENSE-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
 import {Test, console} from "forge-std/Test.sol";
@@ -18,5 +18,10 @@ contract FundMeTest is Test {
     function test_OwnerIsMsgSender() public {
         console.log("i_owner:", fundMe.i_owner());
         assertEq(fundMe.i_owner(), address(this)); /// @dev compare to the address of the test contract because technically it's the msg.sender of fundMe
+    }
+
+    // forge test --match-test test_versionIsAccurate --fork-url $SEPOLIA_URL
+    function test_versionIsAccurate() public {
+        assertEq(fundMe.getVersion(), 4);
     }
 }
