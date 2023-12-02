e97469e01b16a9b6ea6befdcfdb8e9b11ebd5ada
0x18a6 <huffcbytecode@gmail.com>
2023-12-02 16:02:17 +0100

    mainnet config

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -19,6 +19,8 @@ contract HelperConfig {
     constructor() {
         if (block.chainid == 11155111) {
             activeConfig = getSepoliaConfig();
+        } else if (block.chainid == 1) {
+            activeConfig = getMainnetConfig();
         } else {
             activeConfig = getAnvilConfig();
         }
@@ -26,6 +28,11 @@ contract HelperConfig {
 
     function getSepoliaConfig() public pure returns (NetworkConfig memory) {
         NetworkConfig memory config = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        return config;
+    }
+
+    function getMainnetConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory config = NetworkConfig({priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419});
         return config;
     }
 
