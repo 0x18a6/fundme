cdc1900e44b7785973c81d70f0b21b347ea863bd
0x18a6 <huffcbytecode@gmail.com>
2023-12-02 11:51:10 +0100

    basic tests

---
+++ test/FundMe.t.sol
@@ -0,0 +1,22 @@
+// SPDX-LICENSE-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function test_MinUsdIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function test_OwnerIsMsgSender() public {
+        console.log("i_owner:", fundMe.i_owner());
+        assertEq(fundMe.i_owner(), address(this)); /// @dev compare to the address of the test contract because technically it's the msg.sender of fundMe
+    }
+}
