f506e38543b9d28c8c98a9f5778f1dc21200a40e
0x18a6 <huffcbytecode@gmail.com>
2023-12-02 17:55:40 +0100

    constants

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -6,13 +6,17 @@
 pragma solidity ^0.8.23;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
-contract HelperConfig {
+contract HelperConfig is Script {
     // if we're on a local anvil chain, deploy mocks
     // otherwhise, get the existing address from the live network
     struct NetworkConfig {
         address priceFeed;
     }
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
 
     NetworkConfig public activeConfig;
 
@@ -36,5 +40,16 @@ contract HelperConfig {
         return config;
     }
 
-    function getAnvilConfig() public pure returns (NetworkConfig memory) {}
+    function getAnvilConfig() public returns (NetworkConfig memory) {
+        // deploy the mock
+        // return the mock address
+
+        vm.startBroadcast();
+        /// @dev mocking a contract essentially means creating a second version of that contract which behaves very similar to the original one, but in a way that can be easily controlled by the developer
+        MockV3Aggregator mock = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory config = NetworkConfig({priceFeed: address(mock)});
+        return config;
+    }
 }
