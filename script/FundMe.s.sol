b4ed032fa1fb5f638cce323fe7d4398e60434a09
0x18a6 <huffcbytecode@gmail.com>
2023-12-02 15:11:05 +0100

    refactore test

--- script/FundMe.s.sol
+++ script/FundMe.s.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.23;
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
 
-contract FundMeScript is Script {
-    function run() external {
+contract DeployFundMe is Script {
+    function run() external returns (FundMe) {
+        FundMe fundMe;
         vm.startBroadcast();
-        new FundMe();
+        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
+        return fundMe;
     }
 }
--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.23;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/FundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function test_MinUsdIsFive() public {
@@ -17,7 +20,8 @@ contract FundMeTest is Test {
 
     function test_OwnerIsMsgSender() public {
         console.log("i_owner:", fundMe.i_owner());
-        assertEq(fundMe.i_owner(), address(this)); /// @dev compare to the address of the test contract because technically it's the msg.sender of fundMe
+        assertEq(fundMe.i_owner(), address(this));
+        /// @dev compare to the address of the test contract because technically it's the msg.sender of fundMe
     }
 
     // forge test --match-test test_versionIsAccurate --fork-url $SEPOLIA_URL
