1e88bd24e5d2cb1544655a6087ed95dab723e476
0x18a6 <huffcbytecode@gmail.com>
2023-12-16 09:55:37 +0100

    tests

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -8,13 +8,14 @@ import {DeployFundMe} from "../script/FundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
     address USER = makeAddr("user");
+    uint256 public constant VALUE = 69e18;
+    uint256 public constant INITIAL_BALANCE = 100e18;
 
     function setUp() external {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
-
-        vm.deal(USER, 100 ether);
+        vm.deal(USER, INITIAL_BALANCE);
     }
 
     function test_MinUsdIsFive() public {
@@ -22,20 +23,26 @@ contract FundMeTest is Test {
     }
 
     // FIXME: this test is not reverting on my machine
-    function test_fundFailsWithoutEnoughETH() public payable {
-        vm.expectRevert();
-        fundMe.fund{value: 2e18}();
-    }
-
-    // function test_fund() public {
-    //     vm.prank(USER);
+    // function test_RevertWithoutEnoughETH() public payable {
     //     vm.expectRevert();
     //     fundMe.fund{value: 2e18}();
     // }
 
+    function test_RevertWithoutEnoughETH() public {
+        vm.prank(USER);
+        vm.expectRevert();
+        fundMe.fund{value: 2e18}();
+    }
+
+    function test_AddressToAmountFundedIsUpdated() public payable {
+        vm.prank(USER);
+        fundMe.fund{value: VALUE}();
+        assertEq(fundMe.s_addressToAmountFunded(USER), VALUE);
+    }
+
     function test_funderIsUpdated() public payable {
         vm.prank(USER);
-        fundMe.fund{value: 69e18}();
+        fundMe.fund{value: VALUE}();
         assertEq(fundMe.getFunder(0), USER);
     }
 
@@ -48,5 +55,13 @@ contract FundMeTest is Test {
     // forge test --match-test test_versionIsAccurate --fork-url $SEPOLIA_URL
     function test_versionIsAccurate() public {
         assertEq(fundMe.getVersion(), 4);
+    }
+
+    function test_OnlyOwnerCanWithdraw() public payable {
+        vm.startPrank(USER);
+        fundMe.fund{value: VALUE}();
+        vm.expectRevert();
+        fundMe.withdraw();
+        vm.stopPrank();
     }
 }
