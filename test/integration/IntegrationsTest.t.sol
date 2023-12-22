78f8a3d5bd9b9f786df578cc47ca5d9425184cdb
0x18a6 <huffcbytecode@gmail.com>
2023-12-23 00:33:14 +0100

    added integration test

---
+++ test/integration/IntegrationsTest.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/FundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/interactions.s.sol";
+
+contract IntegrationsTest is Test {
+    FundMe fundMe;
+    address USER = makeAddr("user");
+    uint256 public constant VALUE = 69e18;
+    uint256 public constant INITIAL_BALANCE = 100e18;
+
+    function setUp() external {
+        DeployFundMe deploy = new DeployFundMe();
+        fundMe = deploy.run();
+        vm.deal(USER, INITIAL_BALANCE);
+    }
+
+    function testUserCanFundThanOwnerCanWithdraw() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        fundFundMe.fundFundMe(address(fundMe));
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+
+        assert(address(fundMe).balance == 0);
+    }
+}
